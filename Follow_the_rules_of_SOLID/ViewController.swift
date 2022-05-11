b99dda552640c91e8b2c8f71e2dd814178729d82
minjiLee68 <dlalswl2598@gmail.com>
2022-05-12 01:28:21 +0900

    Initial Commit

---
+++ Follow_the_rules_of_SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Follow_the_rules_of_SOLID
+//
+//  Created by 이민지 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
